da8c55fcb48ebf49f71336bfa60f32a9d5cfeb5d
kun <kun2009@foxmail.com>
2017-03-23 00:10:27 +0800

    增加了table对象

---
+++ PowerPointPlus/PowerPointPlus/TableCellInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerPointPlus
+{
+    public class Table
+    {
+        public Rect RectArea { get; set; }
+        public Row RowHeader { get; set; }
+        public List<Row> RowData { get; set; }
+
+        public List<long> ColWidths { get; set; }
+    }
+
+    public class Row
+    {
+        public List<Cell> RowData { get; set; }
+        public long Height { get; set; }
+    }
+
+    public class Cell:TextData
+    {
+        public long Height { get; set; }
+        public long Width { get; set; }
+
+        /// <summary>
+        /// 单元格合并(行)
+        /// </summary>
+        public int RowSpan { get; set; }
+    }
+}
